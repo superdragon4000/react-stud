138135f92568dc26685fafe6769c76a07af5c437
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2024-07-25 20:38:23 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  let likes = 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes}</h1>
+      <button onClick={() => {likes +=1}}>Increment</button>
+      <button onClick={() => {likes -=1}}>Decrement</button>
     </div>
   );
 }
