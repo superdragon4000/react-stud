1d0a92f9eac9b77e7817bdb7eac79607532cdd1d
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2024-08-10 22:56:11 +0300

    feat: postById page, comments

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Posts from "../pages/Posts";
 import About from "../pages/About";
 import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="posts" element={<Posts />}></Route>
+      <Route path="posts/:id" element={<PostIdPage />}></Route>
       <Route path="about" element={<About />}></Route>
       <Route path="*" element={<Error />}></Route>
     </Routes>
